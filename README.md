789af6a5f1a2f35f1295cbaf51fec5cef39afa20
Dmitry Zaprozhets <dmitry.zaporozhec@openiam.com>
2022-02-13 22:39:33 +0300

    impelement basic algs
    Signed-off-by: Dmitry Zaprozhets <dmitry.zaporozhec@openiam.com>

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # sfedu-ictis-ap-sorting
 Sorting algorithms using C++ for course 'Algorithmization and programming' 
 
+## Implemented algorithms
+
+| Alg | Wiki | Video |
+| --- | ------------- | ----------- |
+| Bubble | [Article at Wiki](https://en.wikipedia.org/wiki/Bubble_sort) | [YouTube link](https://youtu.be/lyZQPjUT5B4) |
+| Quick Sort | [Article at Wiki](https://en.wikipedia.org/wiki/Quicksort) | [YouTube link](https://youtu.be/ywWBy6J5gz8) |
--- main.cpp
+++ main.cpp
@@ -2,17 +2,92 @@
 
 using namespace std;
 
+
+//test data
+
+//utilities algs
 int *newArrayFromTemplate(const int *arr, int size);
 
 void *printArray(const int *arr, int size);
 
-void bubbleSort(int *arr, int size);
-
 void swap(int *a1, int *a2);
 
+//Sorting algs
 void quickSort(int *arr, int size);
 
-void mergeSort(int *arr, int size);
+void selectionSort(int *arr, int size);
+
+void insertionSort(int *arr, int size);
+
+void bubbleSort(int *arr, int size);
+
+// Function to perform a test
+int performTest(int testData[], int expectedData[], int testDataSize, const char *algName,
+                void(*sortAlg)(int *arr, int size));
+/**
+ * Implementation of QuickSort
+ */
+namespace QuickSortNS {
+    int partitioning(int *arr, int lo, int hi);
+
+    void quicksort(int *arr, int low, int high);
+
+    void quicksort(int *arr, int low, int high) {
+        if (low < high) {
+            int p = partitioning(arr, low, high);
+            quicksort(arr, low, p - 1);
+            quicksort(arr, p + 1, high);
+        }
+    }
+
+    int partitioning(int *arr, int lo, int hi) {
+        int p = arr[hi];
+        int i = lo - 1;
+        for (int j = lo; j < hi; j++) {
+            if (arr[j] < p) {
+                swap(&arr[j], &arr[++i]);
+            }
+        }
+        swap(&arr[i + 1], &arr[hi]);
+        return i + 1;
+    }
+}
+
+
+/**
+ * Implementation of Selection Sort
+ */
+namespace SelectionSortNS {
+    int *min(int *arr, int from, int to) {
+        int *minA = nullptr;
+        for (int i = from; i < to; i++) {
+            if (minA == nullptr || arr[i] < *minA) {
+                minA = &arr[i];
+            }
+        }
+        return minA;
+    }
+
+    void sort(int *arr, int size) {
+        for (int i = 0; i < size; i++) {
+            int *minA = min(arr, i, size);
+            swap(&arr[i], minA);
+        }
+    }
+}
+
+void insertionSort(int arr[], int size) {
+    int i, key, j;
+    for (i = 1; i < size; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[++j] = key;
+    }
+}
 
 int testResult(const int *expected, const int *current, int size);
 
@@ -34,9 +109,9 @@ int *newArrayFromTemplate(const int *arr, const int size) {
 
 void *printArray(const int *arr, const int size) {
     for (int i = 0; i < size; i++) {
-        printf("%d, ", arr[i]);
+        cout << arr[i] << ", ";
     }
-    printf("\n");
+    cout << endl;
     return nullptr;
 }
 
@@ -46,6 +121,9 @@ void deleteArr(const int *arr) {
 
 
 void swap(int *a1, int *a2) {
+    if (a1 == nullptr || a2 == nullptr || a1 == a2) {
+        return;
+    }
     int tmp = *a1;
     *a1 = *a2;
     *a2 = tmp;
@@ -61,21 +139,40 @@ void bubbleSort(int *arr, int size) {
     }
 }
 
-const int TEST_ARRAY[] = {4, 2, 1, 5, 1, 6, 1, 3, 5, 6, 8, 9, 12, 66, 11, 6, 22, 123, 0, 98};
-const int CHECK_ARRAY[] = {0, 1, 1, 1, 2, 3, 4, 5, 5, 6, 6, 6, 8, 9, 11, 12, 22, 66, 98, 123};
-const int TEST_ARRAY_SIZE = 20;
+void quickSort(int *arr, int size) {
+    QuickSortNS::quicksort(arr, 0, size - 1);
+}
 
+void selectionSort(int *arr, int size) {
+    SelectionSortNS::sort(arr, size);
+}
+
+
+int performTest(int testData[], int expectedData[], int testDataSize, const char *algName,
+                void(*sortAlg)(int *arr, int size)) {
+    int *arr = newArrayFromTemplate(testData, testDataSize);
+    sortAlg(arr, testDataSize);
+    int check = testResult((int *) expectedData, arr, testDataSize);
+    deleteArr(arr);
+
+    if (check != 0) {
+        cerr << "[FAIL] " << algName << " is incorrect. Number of errors " << check << endl;
+        return 1;
+    } else {
+        cout << "[OK] " << algName << " passed Successfully" << endl;
+        return 0;
+    }
+}
 
 int main() {
-    int *arr = newArrayFromTemplate(TEST_ARRAY, TEST_ARRAY_SIZE);
-    printArray(arr, TEST_ARRAY_SIZE);
-    bubbleSort(arr, TEST_ARRAY_SIZE);
-    printArray(arr, TEST_ARRAY_SIZE);
-    int check = testResult((int *) CHECK_ARRAY, arr, TEST_ARRAY_SIZE);
-    if (check != 0) {
-        cout << "Bubble sort is incorrect. Number of errors " << check;
-        return 1;
-    }
-    deleteArr(arr);
-    return 0;
+    int TEST_ARRAY[] = {4, 2, 1, 5, 1, 6, 1, 3, 5, 6, 8, 9, 12, 66, 11, 6, 22, 123, 0, 98};
+    int CHECK_ARRAY[] = {0, 1, 1, 1, 2, 3, 4, 5, 5, 6, 6, 6, 8, 9, 11, 12, 22, 66, 98, 123};
+    const int TEST_ARRAY_SIZE = 20;
+
+    int result = 0;
+    result += performTest(TEST_ARRAY, CHECK_ARRAY, TEST_ARRAY_SIZE, "Bubble", bubbleSort);
+    result += performTest(TEST_ARRAY, CHECK_ARRAY, TEST_ARRAY_SIZE, "Insertion", insertionSort);
+    result += performTest(TEST_ARRAY, CHECK_ARRAY, TEST_ARRAY_SIZE, "Selection", selectionSort);
+    result += performTest(TEST_ARRAY, CHECK_ARRAY, TEST_ARRAY_SIZE, "Quick", quickSort);
+    return result;
 }
