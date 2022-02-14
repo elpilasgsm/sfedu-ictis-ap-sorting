b8a1a6d00028cd8c180ecb2febfe98b9aab71e5f
elpilasgsm <elpilasgsm@gmail.com>
2022-02-14 16:36:47 +0300

    Add execution wrapping

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-#include "iostream"
+#include <iostream>
+#include <chrono>
+
 
 using namespace std;
-
-
-//test data
+using namespace std::chrono;
 
 //utilities algs
+void performanceTestExecutor(const char *algName, void(*sortAlg)(int *arr, int size), const int *seriesSize,
+                             int numOfSeries);
+
+void saveExecutionTimeToFile(const char *algName, int arrSize, unsigned long timeMS);
+
 int *newArrayFromTemplate(const int *arr, int size);
 
 int testResult(const int *expected, const int *current, int size);
@@ -14,9 +19,16 @@ void *printArray(const int *arr, int size);
 
 void swap(int *a1, int *a2);
 
+int *generateTestArray(int size);
+
+void saveToFile(int arrSize, long timeMS);
+
 // Function to perform a test
 int performTest(int testData[], int expectedData[], int testDataSize, const char *algName,
                 void(*sortAlg)(int *arr, int size));
+
+// Function to perform a test
+int performanceTest(const char *algName, void(*sortAlg)(int *arr, int size));
 
 //Sorting algs
 void quickSort(int *arr, int size);
@@ -106,6 +118,31 @@ void swap(int *a1, int *a2) {
     *a2 = tmp;
 }
 
+
+//TODO implement method to generate Test Array
+int *generateTestArray(int size) {
+    //PUT YOUR CODE HERE
+}
+
+//TODO add timing about alg execution time to corresponding file based on algName
+void saveExecutionTimeToFile(const char *algName, int arrSize, unsigned long timeMS) {
+    //PUT YOUR CODE HERE
+}
+
+void performanceTestExecutor(const char *algName, void(*sortAlg)(int *arr, int size), const int *seriesSize,
+                             int numOfSeries) {
+    for (int i = 0; i < numOfSeries; i++) {
+        int sizeOfArray = seriesSize[i];
+        time_point<system_clock> start = high_resolution_clock::now();
+        int *arr = generateTestArray(sizeOfArray);
+        sortAlg(arr, sizeOfArray);
+        deleteArr(arr);
+        time_point<system_clock> stop = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(stop - start);
+        saveExecutionTimeToFile(algName, sizeOfArray, duration.count());
+    }
+}
+
 //TODO implement Bubble Sort alg based on https://en.wikipedia.org/wiki/Bubble_sort
 void bubbleSort(int *arr, int size) {
 //PUT YOUR CODE HERE
@@ -145,11 +182,22 @@ int main() {
     int TEST_ARRAY[] = {4, 2, 1, 5, 1, 6, 1, 3, 5, 6, 8, 9, 12, 66, 11, 6, 22, 123, 0, 98};
     int CHECK_ARRAY[] = {0, 1, 1, 1, 2, 3, 4, 5, 5, 6, 6, 6, 8, 9, 11, 12, 22, 66, 98, 123};
     const int TEST_ARRAY_SIZE = 20;
+    int TEST_SERIES_SIZE[] = {10, 100, 500, 1000, 5000, 10000, 50000, 100000};
+    int TEST_SERIES_NUM = 8;
 
     int result = 0;
     result += performTest(TEST_ARRAY, CHECK_ARRAY, TEST_ARRAY_SIZE, "Bubble", bubbleSort);
     result += performTest(TEST_ARRAY, CHECK_ARRAY, TEST_ARRAY_SIZE, "Insertion", insertionSort);
     result += performTest(TEST_ARRAY, CHECK_ARRAY, TEST_ARRAY_SIZE, "Selection", selectionSort);
     result += performTest(TEST_ARRAY, CHECK_ARRAY, TEST_ARRAY_SIZE, "Quick", quickSort);
-    return result;
+    if (result > 0) {
+        return result;
+    }
+    //Here is performance test with data collection.
+    performanceTestExecutor("Bubble", bubbleSort, TEST_SERIES_SIZE, TEST_SERIES_NUM);
+    performanceTestExecutor("Insertion", insertionSort, TEST_SERIES_SIZE, TEST_SERIES_NUM);
+    performanceTestExecutor("Selection", selectionSort, TEST_SERIES_SIZE, TEST_SERIES_NUM);
+    performanceTestExecutor("Quick", quickSort, TEST_SERIES_SIZE, TEST_SERIES_NUM);
+
+    return 0;
 }
